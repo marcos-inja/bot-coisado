83f93ebbf4a6ec599a2282b53eb43b6e8f6078c2
marcos-inja <marcosvaas@gmail.com>
2021-07-01 21:16:27 -0300

    retirei o de apagar msgs por enquanto

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ const token = require("./token.js")
 
 const client = new discord.Client()
 
-client.on("ready", ( )=> {
+client.on("ready", () => {
     console.log("online")
 })
 
 client.on("message", (msg) => {
-    if(msg.content.toLocaleLowerCase()=="bom dia"){
+    if (msg.content.toLocaleLowerCase() == "bom dia") {
         msg.channel.send(`Bom dia ${msg.author} ☀️`)
 
-    } else if (msg.content.toLocaleLowerCase()=="boa tarde"){
+    } else if (msg.content.toLocaleLowerCase() == "boa tarde") {
         msg.channel.send(`Boa tarde ${msg.author} 🌬️`)
 
-    } else if (msg.content.toLocaleLowerCase()=="boa noite"){
+    } else if (msg.content.toLocaleLowerCase() == "boa noite") {
         msg.channel.send(`Boa noite ${msg.author} 🌚`)
-}
+    }
+})
+
+client.on("guildMemberAdd", member => {
+    
 })
 
 client.login(token)
