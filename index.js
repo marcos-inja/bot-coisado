c46211a8dda8633b54d1e51e33786c66a14f6edf
marcos-inja <marcosvaas@gmail.com>
2021-07-09 20:29:31 -0300

    novos comandos

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const discord = require('discord.js')
-const config = require('./config.json')
+const {prefix, token} = require('./config.json')
 
 const client = new discord.Client()
 
-client.on("ready", () => {
-    console.log("online")
+client.on('ready', () => {
+    console.log('online')
 })
 
-client.on('message', (msg) => {
-    if (msg.content.toLocaleLowerCase() == 'bom dia') {
+client.on('message', msg => {
+    
+    if (msg.content.startsWith('bom dia')) {
         msg.channel.send(`Bom dia ${msg.author} ☀️`)
 
-    } else if (msg.content.toLocaleLowerCase() == 'boa tarde') {
+    } else if (msg.content.startsWith('boa tarde')) {
         msg.channel.send(`Boa tarde ${msg.author} 🌬️`)
 
-    } else if (msg.content.toLocaleLowerCase() == 'boa noite') {
+    } else if (msg.content.startsWith('boa noite')) {
         msg.channel.send(`Boa noite ${msg.author} 🌚`)
 
-    } else if (msg.content.toLocaleLowerCase() == 'olá bot') {
+    } else if (msg.content.startsWith('olá bot')) {
         msg.channel.send(`Olá ${msg.author}!`)
+
+    } else if (msg.content === `${prefix}server`) {
+		msg.channel.send(`O nome do server é: ${msg.guild.name}`)
     }
 })
 
-client.login(config.token)
+client.login(token)
